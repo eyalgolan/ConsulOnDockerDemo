2de550658a1269b188c481ea03030979056504e4
Eyal Golan <eyalgolan96@gmail.com>
2019-10-25 17:28:39 +0300

    commit project

---
+++ httpd_consul/my-key-handler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "New event: ${SERF_EVENT}. Data follows..."
+
+while read line; do
+{
+    printf "${line}\n"
+    printf "${line}\n" >> /var/www/html/eventlog.txt
+}
+
+curl http://172.17.4.161:8500/v1/kv/SE-Funnel/index.html?raw > /var/www/html/index.html
+done
